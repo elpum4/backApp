172ef257b976d2b12ed34f7355bc577167ff3a5a
elpum4 <franckocer@gmail.com>
2022-03-29 15:34:25 -0300

    hk

--- src/main/java/com/oneApp/backApp/BackAppApplication.java
+++ src/main/java/com/oneApp/backApp/BackAppApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackAppApplication {
 
+        public static String PORT = System.getenv("PORT");
+        public static String SERVER_URL = System.getenv("SERVER_URL");
 	public static void main(String[] args) {
 		SpringApplication.run(BackAppApplication.class, args);
 	}
