b2d44968df6f06ca1e3f9371bb69068b0a37f457
elpum4 <franckocer@gmail.com>
2022-03-29 14:04:06 -0300

    hk

--- src/main/java/com/oneApp/backApp/controller/TimeController.java
+++ src/main/java/com/oneApp/backApp/controller/TimeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1.0")
+@RequestMapping("/api")
 public class TimeController {
 
     @GetMapping("/time")
