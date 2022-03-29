046987589b441afc704a0cc7df978762ae26111f
elpum4 <franckocer@gmail.com>
2022-03-29 16:10:52 -0300

    hk

--- src/main/java/com/oneApp/backApp/BackAppApplication.java
+++ src/main/java/com/oneApp/backApp/BackAppApplication.java
@@ -2,12 +2,20 @@ package com.oneApp.backApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class BackAppApplication {
+@ComponentScan
+public class BackAppApplication extends SpringBootServletInitializer {
 
-        public static void main(String[] args) {
+        public static void main(final  String[] args) {
 		SpringApplication.run(BackAppApplication.class, args);
 	}
         
+         @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BackAppApplication.class);
+        }
 }
