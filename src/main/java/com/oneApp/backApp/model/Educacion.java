1a4f85afec4682df6a65ddc8817bf849f4a4c270
elpum4 <franckocer@gmail.com>
2022-03-29 11:40:19 -0300

    heroku

--- src/main/java/com/oneApp/backApp/model/Educacion.java
+++ src/main/java/com/oneApp/backApp/model/Educacion.java
@@ -18,6 +18,7 @@ public class Educacion {
     private String ed_titulo;
     private String ed_descripcion;
     private String ed_institucion;
+    private String ed_urllogo;
     private Date ed_comienzo;
     private Date ed_final;
     private int ed_actual;
@@ -27,16 +28,19 @@ public class Educacion {
     public Educacion() {
     }
 
-    public Educacion(Long id, String ed_titulo, String ed_descripcion, String ed_institucion, Date ed_comienzo, Date ed_final, int ed_actual, int ed_tipo) {
+    public Educacion(Long id, String ed_titulo, String ed_descripcion, String ed_institucion, String ed_urllogo, Date ed_comienzo, Date ed_final, int ed_actual, int ed_tipo) {
         this.id = id;
         this.ed_titulo = ed_titulo;
         this.ed_descripcion = ed_descripcion;
         this.ed_institucion = ed_institucion;
+        this.ed_urllogo = ed_urllogo;
         this.ed_comienzo = ed_comienzo;
         this.ed_final = ed_final;
         this.ed_actual = ed_actual;
         this.ed_tipo = ed_tipo;
     }
+
+ 
 
       
     
--- src/main/java/com/oneApp/backApp/model/Experiencia.java
+++ src/main/java/com/oneApp/backApp/model/Experiencia.java
@@ -18,6 +18,7 @@ public class Experiencia {
     private String exp_titulo;
     private String exp_descripcion;
     private String exp_sitio;
+    private String ex_urllogo;
     private Date exp_comienzo;
     private Date exp_final;
     private int exp_actual;
@@ -27,16 +28,18 @@ public class Experiencia {
     public Experiencia() {
     }
 
-    public Experiencia(Long id, String exp_titulo, String exp_descripcion, String exp_sitio, Date exp_comienzo, Date exp_final, int exp_actual, int exp_tipo) {
+    public Experiencia(Long id, String exp_titulo, String exp_descripcion, String exp_sitio, String ex_urllogo, Date exp_comienzo, Date exp_final, int exp_actual, int exp_tipo) {
         this.id = id;
         this.exp_titulo = exp_titulo;
         this.exp_descripcion = exp_descripcion;
         this.exp_sitio = exp_sitio;
+        this.ex_urllogo = ex_urllogo;
         this.exp_comienzo = exp_comienzo;
         this.exp_final = exp_final;
         this.exp_actual = exp_actual;
         this.exp_tipo = exp_tipo;
     }
 
+    
     
 }
