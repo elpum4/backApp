df27ec0e6428600653f8f84c6dbfffb9fc12c756
elpum4 <franckocer@gmail.com>
2022-04-28 09:34:45 -0300

    basenueva

--- src/main/java/com/oneApp/backApp/model/profile.java
+++ src/main/java/com/oneApp/backApp/model/profile.java
@@ -1,14 +1,9 @@
 package com.oneApp.backApp.model;
 
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
